91d24b1d2fd0f6ec8f1fe05dd4dd8ff54de570f1
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-27 16:38:26 -0500

    app layout and server

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const configureMiddleware = require('./configure-middleware.js');
+
+const server = express();
+
+configureMiddleware(server);
+
+module.exports = server;
