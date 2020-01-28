8e6ff2cb18e2bc2be553d7e36a4c165f25c8983e
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-28 16:43:54 -0500

    cookies and sessions

--- api/server.js
+++ api/server.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session"); 
+const knexSessionStore = require("connect-session-knex")(session);
 
-const apiRouter = require("./api-router.js")
-const configureMiddleware = require('./configure-middleware.js');
+const db = require("../database/dbConfig.js");
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/user-router.js");
 
 const server = express();
 
-configureMiddleware(server);
+const sessionConfig = {
+    name: "cooookies",
+    // secret is used for cookie encryption
+    secret: process.env.SESSION_SECRET || "shhh...keep it a secret",
+    cookie: {
+        maxAge: 1000 * 60 * 10, // 10 minutes in ms
+        secure: false, // set to true in production, only send cookies over HTTPS
+        httpOnly: true, // JS cannot access the cookies on the browser
+    }, 
+    resave: false,
+    saveUninitialized: true, // read up about this! GDPR compliance
+    store: new knexSessionStore({ 
+        knex: db,
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 60000,
+    }),
+};
 
-server.use("/api", apiRouter);
+server.use(helmet());
+server.use(session(sessionConfig)); // turn on sessions
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+server.get("/", (req, res) => {
+    res.json({ api: "it's aliiiiive!" });
+});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,9 +7,7 @@ const bc = require("bcryptjs");
 // for endpoints beginning with /api/auth
 router.post("/register", (req, res) => {
   let user = req.body;
-
-  let hash = bc.hashSync(req.body.password, 8);
-
+  let hash = bc.hashSync(user.password, 10);
   user.password = hash;
 
   Users.add(user)
@@ -28,6 +26,9 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bc.compareSync(password, user.password)) {
+        req.sessions.loggedIn = true; // used in restricted middleware
+        req.session.userId = user.id; // in case we need the user id later
+
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
@@ -38,19 +39,35 @@ router.post("/login", (req, res) => {
     });
 });
 
-router.get("/users", (req, res) => {
-  const username = req.headers.username // headers is similar to local storage
-  // If front end set it to not look for username in the header, this would not work 
-  console.log(username);
+// router.get("/users", (req, res) => {
+//   const username = req.headers.username // headers is similar to local storage
+//   // If front end set it to not look for username in the header, this would not work
+//   console.log(username);
 
-  Users.find()
-    .then(user => {
-      if (username) {
-        res.status(200).json(user);
+//   Users.find()
+//     .then(user => {
+//       if (username) {
+//         res.status(200).json(user);
+//       } else {
+//         res.status(401).json({ message: "You shall not pass!" });
+//       }
+//     });
+// });
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({
+          thanks: "for using our app!"
+        });
       } else {
-        res.status(401).json({ message: "You shall not pass!" });
+        res.status(200).json({ bye: "have a nice day!" });
       }
     });
+  } else {
+    res.status(204);
+  }
 });
 
 module.exports = router;
